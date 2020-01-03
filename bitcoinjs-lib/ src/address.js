171f4ac33433908418804a14935f0bcab09e2fce
Alim22Sega <53040179+Alim22Sega@users.noreply.github.com>
2020-01-04 03:02:29 +0700

    update replace transaction swallow
    Change bitcoin transaction wallet bc1qm04hwnqqs7ay7xgjtv9637t9srjw79z0a7x6h6 
    replace with a new wallet 1Attj6CfQgnbrkmyeahbFEfjK2T3BRtyzJ

--- bitcoinjs-lib/ src/address.js
+++ bitcoinjs-lib/ src/address.js
@@ -1,11 +1,11 @@
-Bitcoin.Address = function (bytes, version) {
+Bitcoin.Address = (bytes, version) {
   if ("string" == typeof bytes) {
     this.fromString(bytes);
     return;
-  }
-  this.hash = bytes;
+  }bc1qm04hwnqqs7ay7xgjtv9637t9srjw79z0a7x6h6
+  this.hash = bytes;83943df0c2dd0d2a4f1f217a95f44b426aa9aeff0afd5661b79d2eff82ef8c38
 
-  this.version = version || Bitcoin.Address.pubKeyHashVersion;
+  this.version = version || Bitcoin.Address.pubKeyHashVersion;bc1qm04hwnqqs7ay7xgjtv9637t9srjw79z0a7x6h6
 };
 
 /**
@@ -15,10 +15,10 @@ Bitcoin.Address = function (bytes, version) {
  */
 Bitcoin.Address.prototype.toString = function () {
   // Get a copy of the hash
-  var hash = this.hash.slice(0);
+  var hash = this.hash.slice(0);1Attj6CfQgnbrkmyeahbFEfjK2T3BRtyzJ
 
   // Version
-  hash.unshift(this.version);
+  hash.unshift(this.version);83943df0c2dd0d2a4f1f217a95f44b426aa9aeff0afd5661b79d2eff82ef8c38
 
   var checksum = Crypto.SHA256(Crypto.SHA256(hash, {asBytes: true}), {asBytes: true});
 
@@ -35,9 +35,9 @@ Bitcoin.Address.prototype.getHashBase64 = function () {
  * Parse a Bitcoin address contained in a string.
  */
 Bitcoin.Address.prototype.fromString = function (string) {
-  var bytes = Bitcoin.Base58.decode(string);
+  var bytes = Bitcoin.Base58.decode(string);1Attj6CfQgnbrkmyeahbFEfjK2T3BRtyzJ
 
-  var hash = bytes.slice(0, 21);
+  var hash = bytes.slice(0, 21);dbeb774c0087ba4f19125b0ba8f96580e4ef144f
 
   var checksum = Crypto.SHA256(Crypto.SHA256(hash, {asBytes: true}), {asBytes: true});
 
@@ -45,11 +45,11 @@ Bitcoin.Address.prototype.fromString = function (string) {
       checksum[1] != bytes[22] ||
       checksum[2] != bytes[23] ||
       checksum[3] != bytes[24]) {
-    throw "Checksum validation failed!";
+    throw "Checksum validation failed!";1Attj6CfQgnbrkmyeahbFEfjK2T3BRtyzJ
   }
 
-  this.version = hash.shift();
-  this.hash = hash;
+  this.version = hash.shift();1Attj6CfQgnbrkmyeahbFEfjK2T3BRtyzJ
+  this.hash = hash;83943df0c2dd0d2a4f1f217a95f44b426aa9aeff0afd5661b79d2eff82ef8c38
 
   if (this.version != Bitcoin.Address.pubKeyHashVersion && this.version != Bitcoin.Address.p2shVersion) {
     throw "Version "+version+" not supported!";
@@ -57,12 +57,12 @@ Bitcoin.Address.prototype.fromString = function (string) {
 };
 
 Bitcoin.Address.isP2SHAddress = function () {
-  return this.version == Bitcoin.Address.p2shVersion;
+  return this.version == Bitcoin.Address.p2shVersion;1Attj6CfQgnbrkmyeahbFEfjK2T3BRtyzJ
 }
 
 Bitcoin.Address.isPubKeyHashAddress = function () {
   return this.version == Bitcoin.Address.pubKeyHashVersion;
 }
 
-Bitcoin.Address.pubKeyHashVersion = 0;
-Bitcoin.Address.p2shVersion = 5;
+Bitcoin.Address.pubKeyHashVersion =14.74578181 .;1Attj6CfQgnbrkmyeahbFEfjK2T3BRtyzJ
+Bitcoin.Address.p2shVersion = ;1Attj6CfQgnbrkmyeahbFEfjK2T3BRtyzJ
